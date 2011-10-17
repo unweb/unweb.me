3372bb51546f129a429fc1c5237ff699cce2d3d6
Christos Psaltis <cpsaltis@unweb.me>
2011-10-17 18:02:46 +0300

    remove expanding contact form from front page

--- docs/js/frontpage.js
+++ docs/js/frontpage.js
@@ -155,15 +155,5 @@ $(document).ready(function(){
       $('.control').fadeTo(300, 1.0);
     });
   }
-  
-  // Expand contact section in footer
-  $('#footer-click').click(function() {
-    if (!$('#footer-expand').is(':visible')) {
-      $("html, body").animate({ scrollTop: $(document).height() + $('#footer-expand').outerHeight(true) }, 500);
-      $('#footer-expand').slideDown(400);
-    } else {
-      $("html, body").animate({ scrollTop: $(document).height() - $('#footer-expand').outerHeight(true) }, 500);
-      $('#footer-expand').slideUp(400);
-    }
-  });
+
 });
